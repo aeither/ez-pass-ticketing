480ae9e61d15a71e2b31e4bfbb66a4ac3d52b4c4
aeither <36173828+aeither@users.noreply.github.com>
2024-11-17 05:49:58 +0700

    wip

--- app/world/interact2/page.tsx
+++ app/world/interact2/page.tsx
@@ -97,6 +97,13 @@ export default function CounterPage() {
 
 			<button
 				type="button"
+				onClick={doSomething}
+				className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+			>
+				doSomething
+			</button>
+			<button
+				type="button"
 				onClick={increment}
 				className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
 			>
